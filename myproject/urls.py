7243410ac03478dd27202e4b55a38273e97a5adb
suhail vs <suhailvs0@gmail.com>
2014-05-19 07:36:50 -0700

    Initial commit

---
+++ myproject/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'myproject.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^chat/', include('chatrooms.urls')),
+)
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
